1b7bb261aae6a13ca12cb39e95b113ef9285ffe0
PJaworski <47056812+gerwld@users.noreply.github.com>
2025-02-24 21:02:12 +0100

    added withClickOutside hoc

---
+++ hocs/withClickOutside.tsx
@@ -0,0 +1,59 @@
+import React, { FC, useEffect, useRef, useState, ComponentType } from "react";
+
+export type WithClickOutsideProps = {
+  refElement?: React.RefObject<HTMLDivElement>;
+  ignoreRef?: React.RefObject<HTMLDivElement>;
+  isShow?: boolean;
+  setShow?: (value: boolean) => void;
+  toggleShow?: () => void;
+  blockScroll?: boolean;
+};
+
+const withClickOutside = <P extends object>(
+  WrappedComponent: ComponentType<P>
+): FC<P & WithClickOutsideProps> => {
+  return ({ blockScroll = false, ...props }) => {
+    const [isShow, setShow] = useState(false);
+    const ref = useRef<HTMLDivElement>(null);
+    const ignoreRef = useRef<HTMLDivElement>(null);
+
+    const toggleShow = () => setShow((prev) => !prev);
+
+    useEffect(() => {
+      const onClickOutside = (event: MouseEvent) => {
+        const target = event.target as Node;
+
+        if (
+          ref.current &&
+          !ref.current.contains(target) &&
+          (!ignoreRef.current || !ignoreRef.current.contains(target))
+        ) {
+          setShow(false);
+        }
+      };
+
+      document.addEventListener("mousedown", onClickOutside);
+      return () => document.removeEventListener("mousedown", onClickOutside);
+    }, []);
+
+    useEffect(() => {
+      if (blockScroll) {
+        document.documentElement.style.overflow = isShow ? "hidden" : "";
+        document.documentElement.style.height = isShow ? "100vh" : "";        
+      }
+    }, [isShow, blockScroll]);
+
+    return (
+      <WrappedComponent
+        {...(props as P)}
+        refElement={ref}
+        ignoreRef={ignoreRef}
+        setShow={setShow}
+        isShow={isShow}
+        toggleShow={toggleShow}
+      />
+    );
+  };
+};
+
+export default withClickOutside;
