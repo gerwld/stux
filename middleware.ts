81a6533bcd020658c9e4d5f988139695bed08759
PJaworski <pjaworski.dev@gmail.com>
2025-03-13 20:32:35 +0100

    Added exp. refferer redirect

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,14 @@ import { routing } from "./i18n/routing";
 export default function middleware(req: NextRequest) {
   const url = req.nextUrl;
   const pathname = url.pathname;
+
+   // Check if user is navigating from the Chrome Web Store
+   const referer = req.headers.get("referer");
+   console.log("Referer:", referer ?? "No referer");
+   if (referer === "https://chromewebstore.google.com/detail/igplus-hide-instagram-ree/dbbopjndlaginbghfoibbndhlbpdpapd") {
+     return NextResponse.redirect(new URL("/igp", req.url));
+   }
+ 
   
   // Checks if the path already has a locale
   const localeMatch = pathname.match(/^\/(pl|en|uk|ru|fr|ar|be|de|hi|ja|nl|pt|sp|sv|zh)(\/|$)/);
